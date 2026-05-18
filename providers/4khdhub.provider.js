021d356b67b311f1300f4a314ebe1ee287576550
navneet <navneetkumar0378@gmail.com>
2026-05-18 11:15:02 +0530

    Add getHome test provider

--- providers/4khdhub.provider.js
+++ providers/4khdhub.provider.js
@@ -1,9 +1,41 @@
 export default {
+
   manifest() {
     return {
       id:"4khdhub",
       name:"4KHDHub",
       version:"1.0.0"
     }
+  },
+
+  getHome(){
+
+    return {
+
+      featured:[],
+
+      rows:[
+        {
+          title:"Provider Test",
+
+          items:[
+            {
+              id:"provider-test-1",
+
+              title:"Provider Connected",
+
+              poster:"https://picsum.photos/300/450",
+
+              backdrop:"https://picsum.photos/1280/720",
+
+              type:"movie"
+            }
+          ]
+        }
+      ]
+
+    }
+
   }
+
 }
